d9042a46def664d6407b955b9e1b268a83f9a2a4
SaintRoberto <luna.robertodev@gmail.com>
2024-11-25 20:34:39 -0500

    Actualización de la aplicación para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/dbz/',
+  base: '/dbz/', // URL base para GitHub Pages
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': 'vue/dist/vue.esm-bundler.js', // Alias específico para Vue
+    },
+  },
+});
