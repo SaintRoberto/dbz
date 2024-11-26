f1c27318f0e5580aca1f3d78353a0b3ff518ea2f
SaintRoberto <luna.robertodev@gmail.com>
2024-11-25 22:08:07 -0500

    fix js y css files

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),      
     },
   },
 });
