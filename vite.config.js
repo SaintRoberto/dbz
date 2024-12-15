676aebdd4e4896a7ad24a49e3990e4d8e843494c
SaintRoberto <luna.robertodev@gmail.com>
2024-12-15 11:59:29 -0500

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dbz/', // URL base para GitHub Pages
+  base: '/', // URL base para GitHub Pages
   plugins: [
     vue(),
   ],
